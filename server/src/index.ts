8076d148bc9690798f44d205af333ef3fd837d02
Drakkarrr <drakkar.alpha00@gmail.com>
2024-06-10 17:21:10 -0700

    added mongoose connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,18 @@
 import express, { Response, Request } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import mongoose, { ConnectOptions } from 'mongoose';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI!, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+} as ConnectOptions);
 
 app.get('/test', async (req: Request, res: Response) => {
   res.json({ message: 'Hello World' });

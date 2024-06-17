fbac883cccd1b832ab9b2d0c9532b3dff385048d
Drakkarrr <drakkar.alpha00@gmail.com>
2024-06-17 04:09:24 -0700

    constructed main nav

---
+++ client/src/components/MainNav.tsx
@@ -0,0 +1,13 @@
+import { Button } from './ui/button';
+
+const MainNav = () => {
+  return (
+    <Button
+      variant='ghost'
+      className='font-bold hover:text-orange-500 hover:bg-white'>
+      Login
+    </Button>
+  );
+};
+
+export default MainNav;

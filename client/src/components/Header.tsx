eee28163688d99fb561e0a30acaa4a5ea8d5a099
Drakkarrr <drakkar.alpha00@gmail.com>
2024-06-17 03:55:27 -0700

    added mobilenav and successfully tested

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import MobileNav from './MobileNav';
 
 const Header = () => {
   return (
@@ -10,6 +11,9 @@ const Header = () => {
           className='text-3xl font-bold tracking-tight text-orange-500'>
           FoodickResto
         </Link>
+        <div className='md:hidden'>
+          <MobileNav />
+        </div>
       </div>
     </div>
   );

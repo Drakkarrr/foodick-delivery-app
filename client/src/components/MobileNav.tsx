11fba880f9923b13b522e3f98a171f7f659d7f84
Drakkarrr <drakkar.alpha00@gmail.com>
2024-06-17 03:52:14 -0700

    constructed responsive mobile nav

---
+++ client/src/components/MobileNav.tsx
@@ -0,0 +1,32 @@
+import { Menu } from 'lucide-react';
+import {
+  SheetTrigger,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetDescription,
+  Sheet,
+} from './ui/sheet';
+import { Separator } from './ui/separator';
+import { Button } from './ui/button';
+
+const MobileNav = () => {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Menu className='text-orange-500' />
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Welcome to FoodickResto</SheetTitle>
+          <Separator />
+          <SheetDescription className='flex'>
+            <Button className='flex-1 font-bold bg-orange-500'>Login</Button>
+          </SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileNav;

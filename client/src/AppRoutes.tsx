b3a5f5eeb1245b4e438dd570df02e1af3e613435
Drakkarrr <drakkar.alpha00@gmail.com>
2024-06-17 03:40:23 -0700

    apply layout globa to test

--- client/src/AppRoutes.tsx
+++ client/src/AppRoutes.tsx
@@ -1,9 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
+import Layout from './layouts/Layout';
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path='/' element={<h1>HomePage</h1>} />
+      <Route path='/' element={<Layout>This is Home Page</Layout>} />
       <Route path='/user-profile' element={<h1>UserProfilePage</h1>} />
       <Route path='*' element={<h1>Not Found</h1>} />
     </Routes>

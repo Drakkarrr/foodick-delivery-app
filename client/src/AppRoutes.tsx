f65a427643da20f12cef0e5c15cedb1c7b283601
Drakkarrr <drakkar.alpha00@gmail.com>
2024-06-21 21:19:25 -0700

    hero homepage initial ssection constructed

--- client/src/AppRoutes.tsx
+++ client/src/AppRoutes.tsx
@@ -1,10 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from './layouts/Layout';
+import HomePage from './pages/HomePage';
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path='/' element={<Layout>This is Home Page</Layout>} />
+      <Route
+        path='/'
+        element={
+          <Layout>
+            <HomePage />
+          </Layout>
+        }
+      />
       <Route path='/user-profile' element={<h1>UserProfilePage</h1>} />
       <Route path='*' element={<h1>Not Found</h1>} />
     </Routes>
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,7 +1,12 @@
 const HomePage = () => {
   return (
     <div className='flex flex-col gap-12'>
-      <div></div>
+      <div className='flex flex-col gap-5 py-8 -mt-16 text-center bg-white rounded-md shadow-md'>
+        <h1 className='text-5xl font-bold tracking-tight text-orange-600'>
+          Your Next Favorite Chow Awaits
+        </h1>
+        <span className='text-xl'>Food is just one click away!</span>
+      </div>
     </div>
   );
 };

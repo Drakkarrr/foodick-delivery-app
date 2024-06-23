9bf41275fcd25185874001ac3b7ea0efaec7e35e
Drakkarrr <drakkar.alpha00@gmail.com>
2024-06-23 08:32:03 -0700

    Partial mobile nav contructed

--- client/src/components/MobileNav.tsx
+++ client/src/components/MobileNav.tsx
@@ -1,4 +1,5 @@
-import { Menu } from 'lucide-react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { CircleUserRound, Menu } from 'lucide-react';
 import {
   SheetTrigger,
   SheetContent,
@@ -11,6 +12,8 @@ import { Separator } from './ui/separator';
 import { Button } from './ui/button';
 
 const MobileNav = () => {
+  const { isAuthenticated, loginWithRedirect, user } = useAuth0();
+
   return (
     <Sheet>
       <SheetTrigger>
@@ -18,10 +21,23 @@ const MobileNav = () => {
       </SheetTrigger>
       <SheetContent>
         <SheetHeader className='space-y-3'>
-          <SheetTitle>Welcome to FoodickResto</SheetTitle>
+          <SheetTitle>
+            {isAuthenticated ? (
+              <span className='flex items-center gap-2 font-bold'>
+                <CircleUserRound className='text-orange-500' />
+                {user?.email}
+              </span>
+            ) : (
+              <span>Welcome to FoodickResto</span>
+            )}
+          </SheetTitle>
           <Separator />
           <SheetDescription className='flex'>
-            <Button className='flex-1 font-bold bg-orange-500'>Login</Button>
+            {isAuthenticated ? (
+              <MobileNavLinks />
+            ) : (
+              <Button className='flex-1 font-bold bg-orange-500'>Login</Button>
+            )}
           </SheetDescription>
         </SheetHeader>
       </SheetContent>

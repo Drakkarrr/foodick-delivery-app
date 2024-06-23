fe2e7b44b3c9b44051c756c0a8817c030f77bd9d
Drakkarrr <drakkar.alpha00@gmail.com>
2024-06-23 08:35:03 -0700

    completed mobile navlinks

--- client/src/components/MobileNavLinks.tsx
+++ client/src/components/MobileNavLinks.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from './ui/button';
 
 const MobileNavLinks = () => {
   return (
     <>
-      <Link clas></Link>
+      <Link
+        to='/user-profile'
+        className='flex items-center font-bold bg-white hover:text-orange-500'>
+        My Profile
+      </Link>
+      <Button className='flex items-center px-3 font-bold hover:bg-gray-500'>
+        Logout
+      </Button>
     </>
   );
 };

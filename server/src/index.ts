6915a353eb6c292b109d08697ec6fd17282be7b2
Drakkarrr <drakkar.alpha00@gmail.com>
2024-06-29 17:44:16 -0700

    server entry point complete

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,28 +1,40 @@
-import express, { Response, Request } from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-import mongoose, { ConnectOptions } from 'mongoose';
+import mongoose from 'mongoose';
 import myUserRoute from './routes/MyUserRoute';
+import { v2 as cloudinary } from 'cloudinary';
+import myRestaurantRoute from './routes/MyRestaurantRoute';
+import restaurantRoute from './routes/RestaurantRoute';
+import orderRoute from './routes/OrderRoute';
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log('Connected to database!'));
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 
 app.use(cors());
+
+app.use('/api/order/checkout/webhook', express.raw({ type: '*/*' }));
+
 app.use(express.json());
 
-mongoose
-  .connect(process.env.MONGO_URI!, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  } as ConnectOptions)
-  .then(() => {
-    console.log('Connected to Database');
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+app.get('/health', async (req: Request, res: Response) => {
+  res.send({ message: 'health OK!' });
+});
 
 app.use('/api/my/user', myUserRoute);
+app.use('/api/my/restaurant', myRestaurantRoute);
+app.use('/api/restaurant', restaurantRoute);
+app.use('/api/order', orderRoute);
 
-app.listen(8000, () => {
-  console.log(`Server is running on localhost:8000`);
+app.listen(7000, () => {
+  console.log('server started on localhost:7000');
 });
